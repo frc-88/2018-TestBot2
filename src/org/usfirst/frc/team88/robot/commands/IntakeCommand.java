973ace92e9145848e316e7c7b8d7c331ef8d11e4
tj2 <navtges@gmail.com>
2018-01-15 16:18:05 -0500

    Lots of cool stuff

--- src/org/usfirst/frc/team88/robot/commands/IntakeCommand.java
+++ src/org/usfirst/frc/team88/robot/commands/IntakeCommand.java
@@ -23,7 +23,7 @@ public class IntakeCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	Robot.intake.intakeWheelSpeed(Robot.oi.applyPoly(Robot.oi.getOperatorZ()));
+    	Robot.intake.intakeWheelSpeed(Robot.oi.applyPoly(Robot.oi.getDriverZ()));
     }
 
     // Make this return true when this Command no longer needs to run execute()
